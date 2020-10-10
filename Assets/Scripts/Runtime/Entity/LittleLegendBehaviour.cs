add19fc2f97f5e780270e9a1c9abfa194f2ad3b2
cqtd <cqtd@protonmail.ch>
2020-10-11 03:58:08 +0900

    Tween 추가, 커서이펙트, 에디터 클라동작

--- Assets/Scripts/Runtime/Entity/LittleLegendBehaviour.cs
+++ Assets/Scripts/Runtime/Entity/LittleLegendBehaviour.cs
@@ -1,11 +1,25 @@
-﻿using Bolt;
+﻿using System;
+using System.Runtime.InteropServices;
+using Bolt;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace CQ.LeagueOfLegends.TFT.Network
 {
+	[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
 	public class LittleLegendBehaviour : EntityEventListener<ILittleLegend>
 	{
 		Camera mainCamera;
+		Vector3 lastPosition;
+		public float surfaceOffset = 0.2f;
+
+		[Header("Debug View")] 
+		public float velocityMag_Debug;
+		public float speedMax_Debug;
+		static readonly int pSpeed = Animator.StringToHash("Speed");
+		static readonly int pIsMoving = Animator.StringToHash("IsMoving");
+
+		public NavMeshAgent agent { get; private set; }
 		
 		public override void Attached()
 		{
@@ -14,14 +28,22 @@ namespace CQ.LeagueOfLegends.TFT.Network
 			Debug.Log("Attached");
 			
 			state.SetTransforms(state.Transform, transform);
+			state.SetAnimator(GetComponent<Animator>());
+
+			// need to chk how this works.
+			state.Animator.applyRootMotion = entity.IsOwner;
 			
 			// 카메라 붙이기
-
 			mainCamera = Camera.main;
-			mainCamera.transform.SetParent(this.transform);
-			mainCamera.transform.localPosition = new Vector3(0, 10, -10);
-			mainCamera.transform.localRotation = Quaternion.Euler(Vector3.right*35f);
+			// mainCamera.transform.SetParent(this.transform);
+			// mainCamera.transform.localPosition = new Vector3(0, 10, -10);
+			// mainCamera.transform.localRotation = Quaternion.Euler(Vector3.right*35f);
 			mainCamera.fieldOfView = 45;
+
+			agent = GetComponent<NavMeshAgent>();
+
+			agent.updateRotation = false;
+			agent.updatePosition = true;
 		}
 		
 		public override void SimulateOwner()
@@ -38,8 +60,48 @@ namespace CQ.LeagueOfLegends.TFT.Network
 			
 			if (movement != Vector3.zero)
 			{
-				transform.position += (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
+				// transform.position += (movement.normalized * speed * BoltNetwork.FrameDeltaTime);
 			}
+		}
+
+		void Update()
+		{
+			if (Input.GetMouseButtonDown(1))
+			{
+				Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+				if (!Physics.Raycast(ray, out RaycastHit hit))
+				{
+					return;
+				}
+
+				Vector3 dest = hit.point + hit.normal * surfaceOffset;
+				if (dest != lastPosition)
+				{
+					lastPosition = dest;
+				
+					SetTarget(dest);
+				}
+			}
+
+			float speed = agent.velocity.magnitude;
+			float speedRate = speed / (agent.speed + 0.1f);
+			
+			state.Animator.SetFloat(pSpeed, speedRate);
+			if (speedRate > 0.1f)
+			{
+				state.Animator.SetBool(pIsMoving, true);
+			}
+			else
+			{
+				state.Animator.SetBool(pIsMoving, false);
+			}
+		}
+
+		void SetTarget(Vector3 position)
+		{
+			this.agent.SetDestination(position);
+			
+			CursorEffect.Spawn(position, null);
 		}
 	}
 }
--- Assets/Scripts/Runtime/TeamFightTactis.cs
+++ Assets/Scripts/Runtime/TeamFightTactis.cs
@@ -9,6 +9,7 @@ namespace CQ.LeagueOfLegends.TFT.Network
 	public class TeamFightTactis : GlobalEventListener
 	{
 		readonly List<string> logMessages = new List<string>();
+		
 		GameObject[] cameraPoints = new GameObject[0];
 		int cameraIndex = 0;
 		
@@ -16,25 +17,42 @@ namespace CQ.LeagueOfLegends.TFT.Network
 		{
 			base.SceneLoadLocalDone(scene, token);
 
+			// 에디터에서는 서버로 클라이언트 로직 일부 실행
+#if UNITY_EDITOR
+			SceneLoadLocalDoneClient(scene, token);
+#else
+
 			if (BoltNetwork.IsServer)
 			{
-				cameraPoints = GameObject.FindGameObjectsWithTag("CameraPoint");
-				if (cameraPoints.Length > 0)
-				{
-					Camera.main.transform.position = cameraPoints[cameraIndex].transform.position;
-					Camera.main.transform.rotation = cameraPoints[cameraIndex].transform.rotation;
-				}
+				SceneLoadLocalDoneServer(scene, token);
 			}
 			
 			else if (BoltNetwork.IsClient)
 			{
-				// 스폰 포인트 가져오기
-				GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
-				Vector3 position = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
-			
-				// 볼트 인스턴시에이트
-				BoltNetwork.Instantiate(BoltPrefabs.LittleLegend, position, Quaternion.identity);
+				SceneLoadLocalDoneClient(scene, token);
 			}
+#endif
+
+		}
+
+		void SceneLoadLocalDoneServer(string scene, IProtocolToken token)
+		{
+			cameraPoints = GameObject.FindGameObjectsWithTag("CameraPoint");
+			if (cameraPoints.Length > 0)
+			{
+				Camera.main.transform.position = cameraPoints[cameraIndex].transform.position;
+				Camera.main.transform.rotation = cameraPoints[cameraIndex].transform.rotation;
+			}			
+		}
+
+		void SceneLoadLocalDoneClient(string scene, IProtocolToken token)
+		{
+			// 스폰 포인트 가져오기
+			GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+			Vector3 position = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+			
+			// 볼트 인스턴시에이트
+			BoltNetwork.Instantiate(BoltPrefabs.LittleLegend, position, Quaternion.identity);			
 		}
 		
 		public override void OnEvent(LogEvent evnt)
